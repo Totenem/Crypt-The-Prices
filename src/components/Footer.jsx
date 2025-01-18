86995822baad461ed4c67243d6dd383ee79d1098
Totemn <163722284+Totenem@users.noreply.github.com>
2025-01-18 18:41:37 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function Footer() {
   return (
     <footer className="bg-purple-100 text-purple-800 text-center py-4">
-      <p>&copy; 2023 Crypt The Prices. Made by Totem and powered by CoinLore API.</p>
+      <p>&copy; 2023 Crypt The Prices. Made by Totemn and powered by CoinLore API.</p>
     </footer>
   )
 }

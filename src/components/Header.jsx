181a860c008c4da5531321b45f92112cface74a4
Totemn <163722284+Totenem@users.noreply.github.com>
2025-02-26 22:17:44 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,6 @@ export function Header() {
         <ul className="flex justify-center space-x-4">
           <li><Link to="/" className="flex items-center hover:text-purple-200"><FaHome className="mr-1" /> Home</Link></li>
           <li><Link to="/price-tracker" className="flex items-center hover:text-purple-200"><FaCoins className="mr-1" /> Search & Track</Link></li>
-          <li><Link to="/maintenance" className="flex items-center hover:text-purple-200"><FaChartLine className="mr-1" /> Insights</Link></li>
-          <li><Link to="/blog" className="flex items-center hover:text-purple-200"><FaBlog className="mr-1" /> Blog</Link></li>
         </ul>
       </nav>
     </header>
